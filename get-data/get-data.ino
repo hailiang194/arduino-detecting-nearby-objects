80e866b514a7286429f58a1ca5b06e522d8a2199
= <=>
2019-08-26 23:23:12 +0700

    Update MAX_DIS

--- get-data/get-data.ino
+++ get-data/get-data.ino
@@ -1,11 +1,23 @@
 #include <Servo.h>
-
+#define MAX_DIS 100
 #define INFINITY 10000
 #define TRIG 8
 #define ECHO 7
 #define SERVO 3
 #define TIME_DELAY 100
 Servo servo;
+
+void write_data_at(int angle)
+{
+    servo.write(angle);
+    Serial.print(angle);
+    Serial.print(":");
+    int distance = get_distance();
+    if((distance == 0) || (distance > MAX_DIS))
+      distance = INFINITY;
+    Serial.println(distance);
+    
+}
 
 int get_distance()
 {
@@ -42,27 +54,13 @@ void loop() {
  
     for(int i = 0; i <= 180; i += 2)
   {
-    servo.write(i);
-    Serial.print(i);
-    Serial.print(":");
-    int distance = get_distance();
-    if(distance == 0)
-      distance = INFINITY;
-    Serial.println(distance);
-    
+    write_data_at(i);
     delay(TIME_DELAY);
   }
 
   for(int i = 178; i >= 2; i -= 2)
   {
-    servo.write(i);
-    Serial.print(i);
-    Serial.print(":");
-    int distance = get_distance();
-    if(distance == 0)
-      distance = INFINITY;
-    Serial.println(distance);
-    
+    write_data_at(i);
     delay(TIME_DELAY);
   }
 
