029a35e48ecd9f46f6178c713a5399c314c7756c
= <=>
2019-08-26 23:25:57 +0700

    add radar mode

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import serial
 import time
 import pygame
-import os
 import list_audio
 import information
+import sys
+import radar
 
+mode = ""
+
+if(len(sys.argv) == 2):
+	mode = sys.argv[1].upper()
+
+displaySize = 0
+
+if(mode == "SOUND"):
+	displaySize = radar.SOUND_ONLY_DISPLAY
+elif(mode == "RADAR"):
+	displaySize = radar.RADAR_DISPLAY
+
+screenDisplay = pygame.display.set_mode(displaySize)
+pygame.display.set_caption("RADAR PROJECT")
 
 pygame.mixer.init()
-os.system("cls")
 time.sleep(3)
 
 ser = serial.Serial('COM3',baudrate = 9600, timeout = 1)
@@ -23,11 +37,34 @@ def play_notification(minDistanceAngle, minDistance):
 		while pygame.mixer.music.get_busy():
 			continue
 
+def notify(angle):
+
+	global rawData
+	global start
+
+	if((angle == 0) or (angle == 180)):
+		minDis = min(rawData.values())
+		if(start):
+			start = False
+			return;
+
+		print('-' * 10)
+		for angleRead in rawData.keys():
+			if(rawData[angleRead] == minDis):
+				print("MIN:\na = %d, d = %d" %(angleRead, minDis))
+				play_notification(angleRead, minDis)
+				break
+
 def setup():
 	for i in range(0, 181, 2):
 		rawData[i] = 0
 
 def update():
+
+	for event in pygame.event.get():
+		if(event.type == pygame.QUIT):
+			pygame.quit()
+			quit()
 
 	data = ser.readline()
 	dataStr = data.decode('utf-8')[:-2] #remove '\r\n'
@@ -45,19 +82,17 @@ def update():
 	print("a = %d, d = %d" %(scanAngle, scanDistance))
 
 	rawData[scanAngle] = scanDistance
-	
-	if((scanAngle == 0) or (scanAngle == 180)):
-		minDis = min(rawData.values())
-		if(start):
-			start = False
-			return;
 
-		print('-' * 10)
-		for angle in rawData.keys():
-			if(rawData[angle] == minDis):
-				print("MIN:\na = %d, d = %d" %(angle, minDis))
-				play_notification(angle, minDis)
-				break
+	if(mode == "SOUND"):
+		notify(scanAngle)
+		soundOnly = pygame.image.load("img\\sound-only.jpg")
+		screenDisplay.blit(soundOnly, (0,0))
+	if(mode == "RADAR"):
+		radar.draw_radar(screenDisplay,scanAngle, rawData)
+
+	pygame.display.flip()
+	
+
 
 def main():
 	setup()
